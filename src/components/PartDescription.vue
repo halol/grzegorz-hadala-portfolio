5d8f5327818cb95cdd387d469d1340f03edd4342
Grzegorz Hadala <grzegorz.hadala@gmail.com>
2017-08-26 19:24:56 +0100

    fixed card desc

--- src/components/PartDescription.vue
+++ src/components/PartDescription.vue
@@ -11,14 +11,12 @@
           <div class="card card-inverse" style="margin-bottom: 48px;">
             <div class="card-body">
               <h4 class="card-title">UX &amp; UI</h4>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
               <p class="card-text">Empathy to the users came to very natural. I’m a problem solver and my biggest goal is to make users happy. With more than 10 years of experience in digital design and artistic soul I’ve developed an eye to create beautiful interfaces and smooth and engaging user flows.</p>
             </div>
           </div>
           <div class="card card-inverse"style="margin-bottom: 48px;">
             <div class="card-body">
               <h4 class="card-title">Tech</h4>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
               <p class="card-text">Front-end development and being up to date with new technologies (and tools) are my hobby. I’m also using it to improve and optimize my design and business workflow. I'm building Vue.js/AngularJS prototypes when it's needed. I keep an eye on implementation process and I make sure that engineers are using best solutions and keep their code tidy with KISS and DRY approach.</p>
             </div>
           </div>
@@ -26,7 +24,6 @@
            <div class="card card-inverse">
             <div class="card-body">
               <h4 class="card-title">Business</h4>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
               <p class="card-text">I’m a business thinker. Making users happy is a first, the second one is to make business happy. Building amazing products is for solving peoples struggle but also is making money and reaching designated markets.</p>
             </div>
           </div>
